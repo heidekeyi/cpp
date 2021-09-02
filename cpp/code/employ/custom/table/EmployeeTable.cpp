780669c3debdecd3b1945b6a14cf84dfc54cc508
heidekeyi <1213173752@qq.com>
2021-09-03 00:32:44 +0800

    refactor emply custom

--- cpp/code/employ/custom/table/EmployeeTable.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by 12131 on 2021/9/1.
-//
-
-#include "EmployeeTable.h"
-
-namespace employ::custom::table {
-    int EmployeeTable::valCount{1};
-}
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,63 +1,13 @@
-
-namespace employ::custom::main {
-    void testUtils();
-
-    void testTable();
-
-    void testModel();
-
-    void testUI();
-}
-
-void test() {
-    using namespace employ::custom::main;
-
-    testUtils();
-    testTable();
-    testModel();
-    testUI();
-}
-
-#include "code/employ/custom/Entry.h"
+#include "code/utils/test/TestUtils.h"
+#include "code/employ/custom/test/TestCustom.h"
+#include "code/employ/custom/app/AppCustom.h"
 
 int main() {
-//    test();
-    using employ::custom::entry;
-    entry();
+//    utils::TestUtils{}.streamUtils();
+    employ::custom::TestCustom{}.testController();
+//    employ::custom::AppCustom{}.start();
     return 0;
 }
-
-#include "code/employ/custom/utils/TestUtils.h"
-#include "code/employ/custom/table/TestTable.h"
-#include "code/employ/custom/model/TestModel.h"
-#include "code/employ/custom/ui/TestUI.h"
-
-namespace employ::custom::main {
-    void testUtils() {
-        using namespace utils;
-        TestUtils obj;
-        obj.datetime().timestamp().empty();
-    }
-
-    void testTable() {
-        using namespace employ::custom::table;
-        TestTable obj;
-        obj.amount().employee();
-    }
-
-    void testModel() {
-        using namespace employ::custom::model;
-        TestModel obj;
-        obj.amount().employee();
-    }
-
-    void testUI() {
-        using namespace employ::custom::ui;
-        TestUI obj;
-        obj.menu().employee();
-    }
-}
-
 
 
 
